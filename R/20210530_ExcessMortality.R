9c3fa1e00337811d189677793db9b0f9d19c1a63
Radek <r.panczak@gmail.com>
2021-06-01 10:51:13 +0200

    Table 1 - yearly edition fixes #50
    code from Katrina; using simple year aggregation

---
+++ R/20210530_ExcessMortality.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(openxlsx)
+
+
+data_mort <- readRDS("C:/Users/kmatth/Dropbox/SpanischeGrippe/ExcessMortality/expected_deaths_monthly.RdS")
+head(data_mort)
+
+pandemic_year <- c("1890","1918","1957","2020")
+  
+table_mort <- as.data.frame(data_mort) %>%
+  select(Country,Year, deaths, pop, fit_flu_hc) %>%
+  filter(Year %in% pandemic_year)%>%
+ group_by(Year, Country) %>%
+ summarise(Expected_Mortality=round(sum(fit_flu_hc),0),
+           Deaths_num=sum(deaths),
+           Cum_excess_death=round(sum(deaths)-sum(fit_flu_hc),0),
+           Percent_excess_death=round((Cum_excess_death/Expected_Mortality)*100,1))%>%
+  ungroup()
+
+write.xlsx(table_mort,"C:/Users/kmatth/Dropbox/SpanischeGrippe/ExcessMortality/Excess_Mortalty.xlsx", row.names=FALSE)
+
+# 
+# table_mort_month <- as.data.frame(data_mort) %>%
+#   select(Country,Year, deaths, pop, fit_flu_hc,Month) %>%
+#   filter(Year=="2020") %>%
+#   group_by(Month, Country) %>%
+#   summarise(Expected_Mortality=round(sum(fit_flu_hc),0),
+#             Cum_excess_death=sum(deaths)-Expected_Mortality,
+#             Percent_excess_death=round((Cum_excess_death/Expected_Mortality)*100,1))%>%
+#   ungroup()
+# 
+# write.xlsx(table_mort_month,"C:/Users/kmatth/Dropbox/SpanischeGrippe/ExcessMortality/Excess_Mortalty_Covid19_Month.xlsx", row.names = 
+#              FALSE)
+# 
+# 
