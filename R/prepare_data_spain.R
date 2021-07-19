406138e9d6b5aaa7c2db25373e9b2dfc6ccfbdad
Katarina Luise Matthes <kmatth@iem.uzh.ch>
2021-07-19 12:07:03 +0200

    es_death_month_new corrected

--- R/prepare_data_spain.R
+++ R/prepare_data_spain.R
@@ -5,6 +5,8 @@ library(dplyr)
 death_spain <-  read.xlsx("data-raw/INE/Spain_2020_death.xlsx",sheet=1)
 pop_spain <-  read.xlsx("data-raw/INE/Spain_Population_age_2020.xlsx",sheet=1)
 pop_spain_prev <- read_rds("data/INE/es_pop_year.Rds")
+death_spain_prev <- read_rds("data/INE/es_deaths_month.Rds")
+
 
 pop_spain_total <- pop_spain %>%
   dplyr::summarize(Population=sum(Population,na.rm=TRUE))%>%
@@ -21,7 +23,17 @@ death_spain_monthly <- death_spain %>%
   mutate(Date=ymd(paste0("2020-",Month,"-01")))%>%
   select(Year, Month, Date, Deaths, Population)
 
-write_rds(death_spain_monthly,paste0("data/INE/es_deaths_month_new.Rds"))
+
+death_spain_monthly_new <- death_spain_prev %>%
+  mutate(Month = as.factor(Month),
+         Deaths=as.integer(Deaths),
+         Population=as.integer(Population))%>%
+  filter(!Year==2020)%>%
+  rbind(death_spain_monthly)
+  
+
+
+write_rds(death_spain_monthly_new ,paste0("data/INE/es_deaths_month_new.Rds"))
 
 death_spain_age <- death_spain %>%
   gather(., Month, Deaths, 2:13, factor_key=TRUE) %>%
@@ -42,3 +54,5 @@ pop_spain_new <- pop_spain_prev %>%
 
 
 write_rds(death_spain_age,paste0("data/INE/es_pop_year_new.Rds"))
+
+
